4e5b5cf43e88aa353ac769265f0e543806d75337
Pierre Genthon <genthon.pierre@gmail.com>
2025-02-05 18:15:27 +0100

    get and post recipes

--- api/index.ts
+++ api/index.ts
@@ -5,10 +5,36 @@ export const config = {
 	runtime: "edge",
 };
 
-const app = new Hono().basePath("/api");
+const recipes = [
+	{
+		id: 1,
+		title: "Crêpes",
+		picture:
+			"https://img.cuisineaz.com/660x495/2015/01/29/i113699-photo-de-crepe-facile.jpeg",
+		timeMinutes: 5,
+	},
+	{
+		id: 2,
+		title: "Lasagnes",
+		picture:
+			"https://assets.afcdn.com/recipe/20200408/109520_w1024h1024c1cx1866cy2800cxt0cyt0cxb3732cyb5600.jpg",
+		timeMinutes: 60,
+	},
+];
 
-app.get("/", (c) => {
-	return c.json({ message: "Hello Hono!" });
+const app = new Hono().basePath("/");
+
+app.get("/recipes", (c) => {
+	return c.json(recipes);
+});
+
+app.post("/recipes", async (c) => {
+	const newRecipe = {
+		id: recipes.length,
+		...(await c.req.json()),
+	};
+
+	return c.json(newRecipe);
 });
 
 export default handle(app);
