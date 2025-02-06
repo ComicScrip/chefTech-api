0a94b5c3ecc4edf12d6bbb439846e37cdc27fd14
Pierre Genthon <genthon.pierre@gmail.com>
2025-02-06 01:22:41 +0100

    change name into title

--- db/schema.ts
+++ db/schema.ts
@@ -2,7 +2,7 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const recipes = sqliteTable("recipes", {
 	id: int().primaryKey({ autoIncrement: true }),
-	name: text().notNull().unique(),
+	title: text().notNull().unique(),
 	timeMinutes: int().notNull(),
 	picture: text().notNull(),
 });
